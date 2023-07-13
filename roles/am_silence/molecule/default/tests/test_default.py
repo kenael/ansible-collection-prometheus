30300c14ecdae9dc7a394db5357cdb13d4169225
Bodo Schulz <bodo.schulz@tallence.com>
2023-07-13 10:24:41 +0200

    am_silence: update molecule test

--- roles/am_silence/molecule/default/tests/test_default.py
+++ roles/am_silence/molecule/default/tests/test_default.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+from __future__ import unicode_literals
 
 from ansible.parsing.dataloader import DataLoader
 from ansible.template import Templar
@@ -8,8 +10,10 @@ import os
 
 import testinfra.utils.ansible_runner
 
+HOST = 'instance'
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(HOST)
 
 
 def pp_json(json_thing, sort=True, indents=2):
@@ -21,53 +25,92 @@ def pp_json(json_thing, sort=True, indents=2):
 
 
 def base_directory():
+    """
+    """
     cwd = os.getcwd()
 
-    if('group_vars' in os.listdir(cwd)):
+    if 'group_vars' in os.listdir(cwd):
         directory = "../.."
         molecule_directory = "."
     else:
         directory = "."
-        molecule_directory = "molecule/{}".format(
-            os.environ.get('MOLECULE_SCENARIO_NAME'))
+        molecule_directory = f"molecule/{os.environ.get('MOLECULE_SCENARIO_NAME')}"
 
     return directory, molecule_directory
+
+
+def read_ansible_yaml(file_name, role_name):
+    """
+    """
+    read_file = None
+
+    for e in ["yml", "yaml"]:
+        test_file = f"{file_name}.{e}"
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return f"file={read_file} name={role_name}"
 
 
 @pytest.fixture()
 def get_vars(host):
     """
-
+        parse ansible variables
+        - defaults/main.yml
+        - vars/main.yml
+        - vars/${DISTRIBUTION}.yaml
+        - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+    operation_system = None
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(
-        base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(
-        molecule_dir)
+    if distribution in ['debian', 'ubuntu']:
+        operation_system = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
 
-    defaults_vars = host.ansible("include_vars", file_defaults).get(
-        "ansible_facts").get("role_defaults")
-    vars_vars = host.ansible("include_vars", file_vars).get(
-        "ansible_facts").get("role_vars")
-    molecule_vars = host.ansible("include_vars", file_molecule).get(
-        "ansible_facts").get("test_vars")
+    file_defaults      = read_ansible_yaml(f"{base_dir}/defaults/main", "role_defaults")
+    file_vars          = read_ansible_yaml(f"{base_dir}/vars/main", "role_vars")
+    role_distribution  = read_ansible_yaml(f"{base_dir}/vars/{operation_system}", "role_distribution")
+    file_molecule      = read_ansible_yaml(f"{molecule_dir}/group_vars/all/vars", "test_vars")
+    # file_host_molecule = read_ansible_yaml("{}/host_vars/{}/vars".format(base_dir, HOST), "host_vars")
+
+    defaults_vars      = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars          = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars   = host.ansible("include_vars", role_distribution).get("ansible_facts").get("role_distribution")
+    molecule_vars      = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # host_vars          = host.ansible("include_vars", file_host_molecule).get("ansible_facts").get("host_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
+    # ansible_vars.update(host_vars)
+
+    # print(pp_json(ansible_vars))
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
     result = templar.template(ansible_vars, fail_on_undefined=False)
 
     return result
 
+def test_user(host, get_vars):
+    """
+    """
+    user = get_vars.get("alertmanager_system_user", "alertmanager")
+    group = get_vars.get("alertmanager_system_group", "alertmanager")
 
-@pytest.mark.parametrize("files", [
-    "/usr/share/GeoIP/GeoIP.dat",
-    "/usr/share/GeoIP/GeoIPCity.dat"
-])
-def test_files(host, files):
-    f = host.file(files)
-    assert f.is_file
+    assert host.group(group).exists
+    assert host.user(user).exists
+    assert group in host.user(user).groups
+    assert host.user(user).home == "/nonexistent"
+
+
+def test_service(host, get_vars):
+    service = host.service("alertmanager")
+    assert service.is_enabled
+    assert service.is_running
