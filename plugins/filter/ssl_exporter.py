c2722efce4d96fbd9fcd9a0c2492af3fc1a97913
Bodo Schulz <bodo@boone-schulz.de>
2024-01-01 10:25:37 +0100

    fix bug in rendering of ssl_exporter_default_module

---
+++ plugins/filter/ssl_exporter.py
@@ -0,0 +1,22 @@
+# python 3 headers, required if submitting to Ansible
+
+from __future__ import (absolute_import, print_function)
+import os
+__metaclass__ = type
+
+from ansible.utils.display import Display
+
+display = Display()
+
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'ssl_exporter_modules': self.ssl_exporter_modules,
+        }
+
+    def ssl_exporter_modules(self, data, valid_probers):
+        """
+        """
+        return [k for k,v in data.items() if v.get('prober') in valid_probers]
